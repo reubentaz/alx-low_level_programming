c751bfd9bf38bdbc661eb5e74c801404ad810d47
Reuben Mathenge <reubirush7@gmail.com>
2023-04-01 09:18:40 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,9 +9,9 @@ char *cap_string(char *str)
 {
 	int index = 0;
 
-	while (string[index])
+	while (str[index])
 	{
-		while (!(str[index] >= '0' && str[index] <= 'z'))
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
 		if (str[index - 1] == ' ' ||
 				str[index - 1] == '\t' ||
