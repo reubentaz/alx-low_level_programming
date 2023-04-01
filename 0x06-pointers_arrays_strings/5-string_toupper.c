72072ab29c0d1194a6ccab9d9ab9969c7d4c1205
Reuben Mathenge <reubirush7@gmail.com>
2023-04-01 08:24:56 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters
+ * of a string to uppercase
+ * @n: the string to be changed
+ * Return: 0
+ */
+char *string_toupper(char *)
+{
+	int p;
+
+	p = 0
+	while (n[p]) != '\0'
+	{
+		if (n[p] >= 'a' && n[p] <= 'z')
+			n[p] = n[p] -  32;
+		p++;
+	}
+	return (str);
+}
