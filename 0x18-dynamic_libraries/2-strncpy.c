4bc5c0f5b59c8e447014606614c742acf7452bab
Reuben Mathenge <reubirush14@gmail.com>
2023-08-23 12:23:29 -0700

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies at most an inputted
+ * number  of  bytes from a string src into dest
+ * @dest: the buffer storing the string copy
+ * @src: source string
+ * @n: the maximum number of bytes to be copied from src
+ * Return: a pointer to the resulting string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, src_len = 0;
+
+	while (src[index++])
+		src_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] =  src[index];
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+	return (dest);
+}
